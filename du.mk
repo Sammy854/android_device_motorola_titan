1b29ae1472a1900189392102db004f40f90a4b2a
Sammy854 <sammybiella@gmail.com>
2015-05-10 11:15:33 +0200

    Update du.mk

--- du.mk
+++ du.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common DU stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
